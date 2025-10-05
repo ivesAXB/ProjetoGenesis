70c60e380d9e802f1bf4a64cf2b1bd18ac8c005e
ivesAXB <ivesdearruda@hotmail.com>
2025-10-05 18:08:56 -0400

    feat: Adiciona a pasta src com a classe principal e o plugin.yml

---
+++ src/main/java/br/com/ivesaxb/projetogenesis/ProjetoGenesis.java
@@ -0,0 +1,14 @@
+package br.com.ivesaxb.projetogenesis;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class ProjetoGenesis extends JavaPlugin {
+    @Override
+    public void onEnable() {
+        getLogger().info("O ProjetoGenesis foi ativado com sucesso!");
+    }
+    @Override
+    public void onDisable() {
+        getLogger().info("O ProjetoGenesis foi desativado.");
+    }
+}
